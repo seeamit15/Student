0b54d75a037b6de4177b73c1ccb02c274a29ecf9
amitamsi <amitamsi@AMITAMSI-IN.idc.oracle.com>
2016-08-10 13:16:24 +0530

    School added

---
+++ src/student/School.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student;
+
+/**
+ *
+ * @author amitamsi
+ */
+public class School {
+    
+   private String schoolName;
+   private int schoolNumber;
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    public int getSchoolNumber() {
+        return schoolNumber;
+    }
+
+    public void setSchoolNumber(int schoolNumber) {
+        this.schoolNumber = schoolNumber;
+    }
+   
+   
+    
+}
